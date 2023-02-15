3b410467fab10f92078857ff801d7b28d0f31612
Raphael Chagas <raphael.ochagas@gmail.com>
2023-02-15 00:19:56 -0300

    Add map to show list

--- src/views/basket/Basket.js
+++ src/views/basket/Basket.js
@@ -1,16 +1,18 @@
 import React from "react"
-import { StyleSheet, View } from 'react-native'
+import { ScrollView, StyleSheet, View } from 'react-native'
 import Header from "./components/Header"
 import Details from "./components/Details"
+import Items from "./components/Items"
 
-export default function Basket({ header, details }) {
-    return <>
+export default function Basket({ header, details, items }) {
+    return <ScrollView>
         <Header {...header}/>
 
         <View style={styles.basket}>
             <Details {...details} />
-        </View>
-    </>
+            <Items {...items} />
+        </View >
+    </ScrollView>
 }
 
 const styles = StyleSheet.create({
